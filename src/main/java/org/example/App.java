7d5ed74a866b1ef55ecda0e28663e5b30b7aee56
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-16 14:33:44 +0530

    Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Starting the Petrol engine
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("1. Petrol engine");
+        System.out.println("2. Diesel engine");
+        Car car=new Car();
+        int choice=scanner.nextInt();
+        switch (choice){
+
+            case 1: PetrolEngine petrolEngine=new PetrolEngine();
+                    car.setEngine(petrolEngine);
+                    car.drive();
+                    break;
+            case 2:
+                   DieselEngine dieselEngine=new DieselEngine();
+                   car.setEngine(dieselEngine);
+                   car.drive();
+                   break;
+
+            default:
+                System.out.println("Invalid input !!");;
+        }
+
+        scanner.close();
     }
 }
